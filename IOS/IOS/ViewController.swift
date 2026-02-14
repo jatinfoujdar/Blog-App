26b148982dfe96285a0c91e02911b86133e06475
Jatin Foujdar <jatin321001@gmail.com>
2026-02-14 23:45:07 +0530

    programmatic approach commit

--- IOS/IOS/ViewController.swift
+++ IOS/IOS/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set background color
+        view.backgroundColor = .white
+        
+        // Create a label
+        let helloLabel = UILabel()
+        helloLabel.text = "Hello World"
+        helloLabel.textColor = .black
+        helloLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        helloLabel.textAlignment = .center
+        
+        // Enable Auto Layout
+        helloLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(helloLabel)
+        
+        // Center the label in the view
+        NSLayoutConstraint.activate([
+            helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
 
