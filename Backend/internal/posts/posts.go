00b23040c16598c5458cf095fc72429533e1a7f6
Jatin Foujdar <jatin321001@gmail.com>
2026-03-02 02:16:40 +0530

    Post model commit

--- Backend/internal/posts/posts.go
+++ Backend/internal/posts/posts.go
@@ -1,1 +1,18 @@
 package posts
+
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Post struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title       string             `bson:"title" json:"title"`
+	Subtitle    string             `bson:"subtitle" json:"subtitle"`
+	Content     string             `bson:"content" json:"content"`
+	Category    string             `bson:"category" json:"category"`
+	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt   time.Time          `bson:"updatedAt" json:"updatedAt"`
+}
