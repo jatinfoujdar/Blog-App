68dbc1944aec4cf217a98b8084a616d6318921a5
Jatin Foujdar <jatin321001@gmail.com>
2026-03-10 21:24:08 +0530

    Encapsulation (Cell manages its own UI)

---
+++ IOS/IOS/ViewController/ReminderListViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ReminderListViewController.swift
+//  IOS
+//
+//  Created by jatin foujdar on 06/03/26.
+//
+
+import UIKit
+
+class ReminderListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate{
+    
+    var tasks: [Task] = []
+    
+    private let titleLabel : UILabel = {
+        let lb = UILabel()
+        lb.text = "My Day"
+        lb.textColor = .black
+        lb.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        return lb
+    }()
+    
+    private let tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .plain)
+        return tv
+    }()
+    
+    private let inputStack: UIStackView = {
+           let sv = UIStackView()
+           sv.axis = .horizontal
+           sv.spacing = 10
+           sv.alignment = .fill
+           sv.distribution = .fill
+           return sv
+       }()
+    
+    private let inputField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "New Task"
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
+    
+    private let addButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Add", for: .normal)
+        btn.contentEdgeInsets = UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 16)
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = .systemBlue
+        btn.layer.cornerRadius = 8
+        return btn
+    }()
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           view.backgroundColor = .white
+    
+        
+           setupInputStack()
+           setupTableView()
+           setupConstraints()
+           
+           inputField.delegate = self
+           addButton.addTarget(self, action: #selector(addTask), for: .touchUpInside)
+       }
+    
+    
+    
+    private func setupInputStack() {
+        
+        view.addSubview(titleLabel)
+          inputStack.addArrangedSubview(inputField)
+          inputStack.addArrangedSubview(addButton)
+          view.addSubview(inputStack)
+      }
+      
+      private func setupTableView() {
+          view.addSubview(tableView)
+          tableView.dataSource = self
+          tableView.delegate = self
+          tableView.register(TaskCell.self, forCellReuseIdentifier: TaskCell.identifier)
+      }
+    
+    private func setupConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        inputStack.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            
+            
+            inputStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            inputStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            inputStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            
+            inputField.heightAnchor.constraint(equalToConstant: 40),
+            
+            
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: inputStack.topAnchor, constant: -10)
+        ])
+    }
+
+
+    
+    
+    @objc func addTask() {
+        guard let text = inputField.text, !text.isEmpty else { return }
+        let newTask = Task(
+            id: UUID(),
+            title: text,
+            isCompleted: false
+        )
+        tasks.append(newTask)
+        tableView.reloadData()
+        inputField.text = ""
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TaskCell.identifier, for: indexPath) as? TaskCell else{
+            return UITableViewCell()
+        }
+        let task = tasks[indexPath.row]
+        cell.configure(with: task)
+        return cell
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addTask()
+        inputField.resignFirstResponder()
+        return true
+    }
+}
---
+++ IOS/IOS/Views/TaskCell.swift
@@ -0,0 +1,68 @@
+//
+//  TaskCell.swift
+//  IOS
+//
+//  Created by jatin foujdar on 10/03/26.
+//
+
+import UIKit
+
+class TaskCell : UITableViewCell {
+    
+    static let identifier = "TaskCell"
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let completeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("✓", for: .normal)
+        button.tintColor = .systemGreen
+               return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+           contentView.addSubview(titleLabel)
+           contentView.addSubview(completeButton)
+       }
+       
+    private func setupConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        completeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            completeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            completeButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: completeButton.leadingAnchor, constant: -10)
+        ])
+    }
+       
+    func configure(with task: Task) {
+        titleLabel.text = task.title
+        
+        if task.isCompleted {
+            titleLabel.textColor = .gray
+        } else {
+            titleLabel.textColor = .black
+        }
+    }
+}
